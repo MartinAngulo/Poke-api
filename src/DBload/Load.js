ffc2472b5bdbccf9fa063d5431f70bdf427f374d
Martin Angulo <martinangulo1194@gmail.com>
2022-09-04 12:26:48 -0500

    changes for Azure

--- src/DBload/Load.js
+++ src/DBload/Load.js
@@ -10,23 +10,55 @@ module.exports = {
         await Types.bulkCreate(typesNames);
     },
 
-    loadPokemons: async (start,limit=50) => {
+    loadPokemons: async (start, limit = 50) => {
+
+        // const pokemonName = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${start}&limit=${limit}`)
+        //     .then(response => response.data.results);
+
+
+        //     pokemonName.forEach(async(poke)=>{
+        //     const pokeInfo = await axios.get(poke.url).then(res => res.data);
+        //     const types = pokeInfo.types.map(e=>e.type.name);
+        //     const typesId = await Types.findAll({
+        //         where: {
+        //             name: types
+        //         },
+        //         attributes: ['id']
+        //     }).then((a)=>a.map(e=>e.id));
+
+        //     await Pokemon.create({
+        //         name: pokeInfo.name,
+        //         idPoke: pokeInfo.id,
+        //         health: pokeInfo.stats[0]?.base_stat,
+        //         attack: pokeInfo.stats[1]?.base_stat,
+        //         defense: pokeInfo.stats[2]?.base_stat,
+        //         attackEsp: pokeInfo.stats[3]?.base_stat,
+        //         defenseEsp: pokeInfo.stats[4]?.base_stat,
+        //         speed: pokeInfo.stats[5]?.base_stat,
+        //         heigth: pokeInfo.height,
+        //         weight: pokeInfo.weight,
+        //         img: pokeInfo.sprites.other.home.front_default&&pokeInfo.sprites.other.home.front_default
+        //     })
+        //     .then((pokemon)=>{
+        //         pokemon.addTypes(typesId);
+        //     });
+        // });
 
         const pokemonName = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${start}&limit=${limit}`)
             .then(response => response.data.results);
 
 
-            pokemonName.forEach(async(poke)=>{
+        const pokemonRequests = pokemonName.map(async (poke) => {
             const pokeInfo = await axios.get(poke.url).then(res => res.data);
-            const types = pokeInfo.types.map(e=>e.type.name);
+            const types = pokeInfo.types.map(e => e.type.name);
             const typesId = await Types.findAll({
                 where: {
                     name: types
                 },
                 attributes: ['id']
-            }).then((a)=>a.map(e=>e.id));
+            }).then((a) => a.map(e => e.id));
 
-            await Pokemon.create({
+            const pokemon = await Pokemon.create({
                 name: pokeInfo.name,
                 idPoke: pokeInfo.id,
                 health: pokeInfo.stats[0]?.base_stat,
@@ -37,11 +69,12 @@ module.exports = {
                 speed: pokeInfo.stats[5]?.base_stat,
                 heigth: pokeInfo.height,
                 weight: pokeInfo.weight,
-                img: pokeInfo.sprites.other.home.front_default&&pokeInfo.sprites.other.home.front_default
-            })
-            .then((pokemon)=>{
-                pokemon.addTypes(typesId);
+                img: pokeInfo.sprites.other.home.front_default && pokeInfo.sprites.other.home.front_default
             });
+
+            pokemon.addTypes(typesId);
         });
+
+        return Promise.all(pokemonRequests);
     }
 }
