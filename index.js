9110bee3ecdf9a14708bdd1c9a786e972e2b7717
Martin Angulo <martinangulo1194@gmail.com>
2022-09-04 12:06:49 -0500

    Removing DB initialization from start

--- index.js
+++ index.js
@@ -24,15 +24,15 @@ const Load = require('./src/DBload/Load.js')
 const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ force: true })
-.then(async()=>await Load.loadTypes())
-.then(async()=>{
-  for(let i=0; i<58;i++){
-    await Load.loadPokemons(i*20,20);
-  }
-})
-.then(() => {
+// conn.sync({ force: true })
+// .then(async()=>await Load.loadTypes())
+// .then(async()=>{
+//   for(let i=0; i<58;i++){
+//     await Load.loadPokemons(i*20,20);
+//   }
+// })
+// .then(() => {
   server.listen(port, () => {
     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
-})
+// })
