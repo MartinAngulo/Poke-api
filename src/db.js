7177fd142b289fadb871af837dd264c9b5d226ab
Martin Angulo <martinangulo1194@gmail.com>
2022-10-25 07:28:13 -0500

    fix error pg for deploy vercel

--- src/db.js
+++ src/db.js
@@ -9,6 +9,15 @@ const {
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/xatgverb`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectModule: pg,
+  //options for azure DB:
+  // dialectOptions: {
+  dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false // This line will fix new error
+      }
+    },
 });
 const basename = path.basename(__filename);
 
